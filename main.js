55b2744a55d181732e131ebdd87450ba292401d9
Ruben Henares <henareshidalgoruben@fpllefia.com>
2023-05-19 15:09:52 +0200

    home.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import './style.css'
 
 import * as bootstrap from 'bootstrap'
 
-import { Home } from './vistas/Home'
+import { Home } from './vistas/Home.js'
 
 document.querySelector('main').innerHTML = Home.template
 Home.script()
